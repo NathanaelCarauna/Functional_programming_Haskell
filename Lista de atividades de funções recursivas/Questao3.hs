85fe530b1cdcbd61e6f828afb36fc17e8a9086a3
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-02-28 15:37:05 -0300

    Questão 3 resolvida

---
+++ Lista de atividades de funções recursivas/Questao3.hs
@@ -0,0 +1,11 @@
+-- 3) Defina o operador de exponenciação ^ utilizando uma função recursiva, 
+-- semelhante ao padrão usado para implementar a multiplicação com o operador *:
+-- (*) :: Num a => a -> a -> a
+-- m * 0 = 0
+-- m * n = m + (m * (n - 1))
+
+
+
+(^^^) :: (Eq p, Num p) => p -> p -> p
+m ^^^ 0 = 1
+m ^^^ n = m * (m ^^^ (n-1))
