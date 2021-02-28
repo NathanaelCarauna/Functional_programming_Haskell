47543125fd3fa9f181d0b71898d6f3c27c6a2418
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-02-28 16:47:24 -0300

    Questão 6 resolvida

---
+++ Lista de atividades de funções recursivas/Questao6.hs
@@ -0,0 +1,12 @@
+-- 6) Definir a função recursiva merge :: Ord a => [a] -> [a] -> [a] 
+-- que une duas listas ordenadas em uma lista ordenada. Exemplo:
+-- > merge [2,5,6] [1,3,4]
+-- [1,2,3,4,5,6]
+
+
+
+merge' [] ys = ys
+merge' xs [] = xs
+merge' (x:xs) (y:ys)
+    |x < y = x : merge' xs (y:ys)
+    |otherwise = y : merge' (x:xs) ys
