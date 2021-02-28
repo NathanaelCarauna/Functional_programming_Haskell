91ce03195c4489cb508e4143ef7f673628c6bf2c
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-02-28 13:52:32 -0300

    Questão7 Resolvida

--- Lista de atividades de compreensão de listas/Questao7.hs
+++ Lista de atividades de compreensão de listas/Questao7.hs
@@ -2,7 +2,14 @@
 --  com dois geradores, pode ser representada usando duas compreensões de lista,
 --  cada uma com apenas um gerador. Dica: Procure usar a função concat. 
 
+listaMeta :: [(Integer, Integer)]
 listaMeta = [(x,y) | x <- [1,2], y <- [3,4]]
-lista1 = [x | x <- [1, 2]]
-lista2 = [y | y <- [3, 4]]
+
+lista1 :: [(Integer, Integer)]
+lista1 = [(1, x) | x <- [3, 4]]
+
+lista2 :: [(Integer, Integer)]
+lista2 = [(2, y) | y <- [3,4]]
+
+listaFinal :: [(Integer, Integer)]
 listaFinal = lista1 ++ lista2
